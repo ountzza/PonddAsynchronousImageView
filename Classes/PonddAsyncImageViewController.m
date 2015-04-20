80b93b0bf09f76340dcc0f127191fcc898f34400
Narasimha Raja <snraja@outlook.com>
2015-04-20 21:31:15 +0530

    Changed ViewController

--- Classes/PonddAsyncImageViewController.m
+++ Classes/PonddAsyncImageViewController.m
@@ -10,40 +10,14 @@
 
 @implementation PonddAsyncImageViewController
 
-
-
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-	NSURL *url = [NSURL URLWithString:@"http://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Mona_Lisa.jpeg/401px-Mona_Lisa.jpeg"];
+    NSString *urlString = [NSString stringWithFormat:@"http://dummyimage.com/%dx%d/09e04d/000657.png", (int)self.view.frame.size.width, (int)self.view.frame.size.height];
+	NSURL *url = [NSURL URLWithString: urlString];
 	AsyncImageView *asyncImageView = [[AsyncImageView alloc] initWithFrame:self.view.frame];
-	[asyncImageView loadImageFromURL:url withFileName:@"someUniqueFileName.png" ];
+	[asyncImageView loadImageFromURL:url withFileName: urlString];
 	[self.view addSubview:asyncImageView];
 }
-
-
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
-
 
 @end
